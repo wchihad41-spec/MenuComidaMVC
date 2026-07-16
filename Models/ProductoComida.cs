e22044b670caa30ff3b6faecb30245609b5ad4a7
kyoya mikeylean <www.kursecb@hotmail.com>
2026-07-16 12:28:15 -0600

    Agregar archivos de proyecto.

---
+++ Models/ProductoComida.cs
@@ -0,0 +1,11 @@
+﻿namespace MenuComidaMVC.Models
+{
+    public class ProductoComida
+    {
+        public string Nombre { get; set; } = string.Empty;
+
+        public decimal Precio { get; set; }
+
+        public string Imagen { get; set; } = string.Empty;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Menu/Index");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Menu}/{action=Index}/{id?}");
+
+app.Run();
